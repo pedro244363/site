860e52f9e839070c4b715bbb4dd82a6ed6b06487
Pedro Arthur Pereira de Sousa <pedroarthur.silvs2007@gmail.com>
2025-04-17 21:04:51 -0300

    update de teste

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
-// const é o  objeto icone pra trocar cor do site
-const toggleTheme = document.getElementById("toggleTheme")
-const rootHtlm = document.documentElement;
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement;
+const accordionHeader = document.querySelectorAll(".accordion-header");
 
-function changeTheme() {
+function changeTheme () {
 
-    const currentTheme = rootHtlm.getAttribute("data-theme");
+    const currentTheme = rootHtml.getAttribute("data-theme");
 
-    currentTheme === "dark" ? rootHtlm.setAttribute("data-theme", "light") : rootHtlm.setAttribute("data-theme", "dark");
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
 
-    toggleTheme.classList.toggle("bi-moon-stars")
-    toggleTheme.classList.toggle("bi-sun")
-
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
 }
 
-toggleTheme.addEventListener("click", changeTheme)
+toggleTheme.addEventListener("click", changeTheme);
+
+//function accordion
+accordionHeader.forEach(header => {
+
+    header.addEventListener("click", () => {
+
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+})
