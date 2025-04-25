e6550bf012e22a280624dad365c3f7ff34181879
Pedro Arthur Pereira de Sousa <pedroarthur.silvs2007@gmail.com>
2025-04-25 20:42:20 -0300

    update 25/04

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const toggleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement;
 const accordionHeader = document.querySelectorAll(".accordion-header");
-
+const menuLinks = document.querySelectorAll(".menu-Link")
 function changeTheme () {
 
     const currentTheme = rootHtml.getAttribute("data-theme");
@@ -23,5 +23,12 @@ accordionHeader.forEach(header => {
         const accordionActive = accordionItem.classList.contains("active");
 
         accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+})
+
+menuLinks.forEach(item => {
+    item.addEventListener("Click" , () =>{
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active")
     })
 })
